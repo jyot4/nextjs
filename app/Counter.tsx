3ee87e072e6da75f0f3f08ac686b9867938300e6
jyot4 <jyotivyass2000@gmail.com>
2023-09-06 09:53:22 +0530

    first commit

---
+++ app/Counter.tsx
@@ -0,0 +1,26 @@
+
+
+"use client"
+
+import React,{useState} from 'react'
+
+
+function Counter() {
+const [add,setAdd] = useState(0)
+
+    function inc(){
+        setAdd ( add + 1)
+    }
+    function dec(){
+        setAdd( add - 1)
+    }
+  return (
+    <div>
+      <button onClick={inc}>increament</button>
+      <p>{add}</p>
+      <button onClick={dec}>decrement</button>
+    </div>
+  )
+}
+export const useClient = true;
+export default Counter
